99f518c5243ce8ded48819c65b54ab70d8ca5d6c
HikmetMehraliyev <7830uh8@code.edu.az>
2023-04-04 04:50:05 +0400

    HomeTaskDoWhile

---
+++ FirstTask/Program.cs
@@ -0,0 +1,49 @@
+﻿#region(While)
+
+//int num = 15;
+
+//while (9<num && num<21)
+//{
+//    while (num%2==0)
+//    {
+//        Console.WriteLine("Cüt ədəddir");
+//        break;
+//    }
+//    Console.WriteLine("Tək ədəddir");
+//    break;
+//}
+//while (9>=num || num>=21)
+//{
+//  Console.WriteLine("Ədəd bura daxil deyil");
+//    break;
+//}
+#endregion
+
+#region(Do/While)
+//int num = 16;
+
+//do
+//{
+//	while (9 < num && num < 21)
+//	{
+//		while (num%2==0)
+//		{
+//            Console.WriteLine("Cüt ədəddir");
+//			break;
+//        }
+//		while (num%2!=0) 
+//		{
+//            Console.WriteLine("Tək ədəddir");
+//            break;
+    
+//		}
+//        break;
+//    }
+	
+//} while (false);
+
+//while (9 >= num || num >= 21) 
+//{
+//  Console.WriteLine("Ədəd bura daxil deyil");
+//}
+#endregion
